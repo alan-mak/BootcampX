76944a52a99c833bc644c26a3ab1f5cf18b47f6b
Alan <alanmak95@gmail.com>
2021-01-04 18:02:14 +0000

    Initialize project beginnings

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS cohorts;
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT,
+  phone TEXT,
+  github TEXT,
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
